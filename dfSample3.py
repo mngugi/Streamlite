47d43f06177409dbbde00d5a723c3510f41ee68c
M.ngugi <mngugi7@gmail.com>
2024-08-01 14:24:13 +0300

    Update dfSample3.py

--- dfSample3.py
+++ dfSample3.py
@@ -2,10 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-df = pd.DataFrame(
-np.random.randn(10,20),
-columns = ('col %d' % i for i in range(20)))
-
-st.table(dataframe)
-
+# Generate a random dataframe with 10 rows and 20 columns
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20))
 )
+
+# Use Streamlit to display the dataframe with highlighted maximum values in each column
+st.dataframe(dataframe.style.highlight_max(axis=0))
+
