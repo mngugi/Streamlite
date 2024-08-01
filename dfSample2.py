ea941376cbe955618a615f5f640d63660b2ebb26
M.ngugi <mngugi7@gmail.com>
2024-08-01 13:31:49 +0300

    Update dfSample2.py

--- dfSample2.py
+++ dfSample2.py
@@ -1,9 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-st.write("Sample Table for a 2 X 6 matrix solution")
+st.write("Sample Table for a 3 X 6 matrix solution")
 st.write(pd.DataFrame({
-
+'Name' : ("Tom","Curt","Dog","Piet","Man","Honk"),
 'x' : [56, 58, 54, 60, 64,65],
 'y' : [1, 2, 3, 4, 5,65]
 
