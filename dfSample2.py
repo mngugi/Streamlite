56f1f3868e00084bbfc74686d585b4d9054091ee
M.ngugi <mngugi7@gmail.com>
2024-08-01 13:43:01 +0300

    Update dfSample2.py

--- dfSample2.py
+++ dfSample2.py
@@ -5,7 +5,7 @@ st.write("Sample Table for a 3 X 6 matrix solution")
 st.write(pd.DataFrame({
 'Name' : ("Tom","Curt","Dog","Piet","Man","Honk"),
 'x' : [56, 58, 54, 60, 64,65],
-'y' : [1, 2, 3, 4, 5,65]
+'y' : [1, 2, 3, 4, 5, 6]
 
 }))
 
