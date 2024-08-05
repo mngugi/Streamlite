baaeaeaeb277ad7a10bab166b5578bb9d5043c7b
M.ngugi <mngugi7@gmail.com>
2024-08-05 12:51:35 +0300

    Update sunburst_chart.py

--- sunburst_chart.py
+++ sunburst_chart.py
@@ -1,15 +1,21 @@
-import plotly.express as px
 import streamlit as st
+import plotly.express as px
 import pandas as pd
 
+# Data
 data = {
-    'userName': [ "Angel", "Ben", "Cat", "Dougie", "Eve", "Father", "Gloria"],
-    'Value': [10,15,9,18,20,10,13],
+    'userName': ["Angel", "Ben", "Cat", "Dougie", "Eve", "Father", "Gloria"],
+    'Value': [10, 15, 9, 18, 20, 10, 13],
+}
 
-         }
+# Convert the data dictionary to a pandas DataFrame
 df = pd.DataFrame(data)
-figure = px.sunburst(data, names = 'userName', values= 'Value')
 
-figure.update_layout(title_text= "Sunburst Chart ")
+# Create a sunburst chart using Plotly
+fig = px.sunburst(df, path=['userName'], values='Value')
 
-st.plotly_chart(figure)
+# Update the layout of the figure
+fig.update_layout(title_text="Sunburst Chart")
+
+# Display the sunburst chart in Streamlit
+st.plotly_chart(fig)
