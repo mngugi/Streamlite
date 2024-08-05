89717914245d96d154a13022785c986fa923ff60
M.ngugi <mngugi7@gmail.com>
2024-08-05 13:37:55 +0300

    Update sunburst_chart.py

--- sunburst_chart.py
+++ sunburst_chart.py
@@ -17,7 +17,7 @@ st.dataframe(df)
 fig = px.sunburst(df, path=['userName'], values='Value')
 
 # Update the layout of the figure
-fig.update_layout(title_text="Sunburst Chart")
+fig.update_layout(title_text="### Sunburst Chart")
 
 # Display the sunburst chart in Streamlit
 st.plotly_chart(fig)
