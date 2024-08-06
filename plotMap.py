bdc48ba1ba6068f91945ee0b5789162ccd338e38
M.ngugi <mngugi7@gmail.com>
2024-08-07 00:44:38 +0300

    Update plotMap.py

--- plotMap.py
+++ plotMap.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 mapData = pd.DataFrame(
-    np.random.randn(1000, 2) / [30,50] + [30.76, -233.4],
+    np.random.randn(2000, 2) / [30,30] + [30.86, -133.4],
     columns=['lat', 'lon']
     )
 
