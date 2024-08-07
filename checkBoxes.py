a72f2a732c764660a32d30fcad9b4fe9b09a170d
M.ngugi <mngugi7@gmail.com>
2024-08-07 22:43:50 +0300

    Update checkBoxes.py

--- checkBoxes.py
+++ checkBoxes.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-if st.checkbox('Show dataframe')
+if st.checkbox('Show dataframe'):
     chart_data = pd.DataFrame(
         np.random.randn(20,3),
         columns = ['apples','bananas', 'oranges']
