1cd28b5279dbd19d7ae0e9cd44cd800ec0bda77c
M.ngugi <mngugi7@gmail.com>
2024-08-07 11:17:27 +0300

    Update createAmap.py

--- createAmap.py
+++ createAmap.py
@@ -1,7 +1,16 @@
 import folium
 from IPython.display import display
 
+map_center = [40.7128, +74.0068]
 
+map_loc = folium.Map(location=map_center, zoom_start=12)
+
+
+folium.Marker( [40.7128, +74.0060],
+               popup="New York",
+               icon=folium.Icon(color="blue", icon="info-sign")).add_to(map_loc)
+
+display(map_loc)
 
 
 
