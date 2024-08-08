50fb3c71ac6cfd51be9d4464b4a37a7a05806af0
M.ngugi <mngugi7@gmail.com>
2024-08-08 16:06:05 +0300

    Update layout.py

--- layout.py
+++ layout.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-add_selectbox = st.sidebar.selectbox('Select Contact mode', ('Email', 'Work phone', 'Cell Phone'))
+add_selectbox = st.sidebar.selectbox('Select Contact mode', ('Email', 'Work phone', 'Cell Phone', ' Repository'))
 
 add_slider = st.sidebar.slider('Select range of values', 0.0,100.0,(25.0,75.0))
 
