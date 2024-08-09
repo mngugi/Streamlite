602c823c5e0cb5771f06692a6ebd6737b5517c61
M.ngugi <mngugi7@gmail.com>
2024-08-10 00:18:00 +0300

    Update pages2.py

--- pages2.py
+++ pages2.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-st.markdown("Main Page")
-st.sidebar.markdown("Main Page")
+st.markdown("# Main Page")
+st.sidebar.markdown("# Main Page")
 
 st.markdown("# Page 2 *")
 st.sidebar.markdown("# Page 2 *")
+
+st.markdown("# Page 3 *")
+st.sidebar.markdown("# Page 3 *")
+
